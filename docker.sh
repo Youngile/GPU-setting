dab15353bee3c9f466e430c6bf778ddda4fd0405
Youngile <https://github.com/Youngile>
2024-10-18 13:32:38 +0900

    GPU-SETTING

---
+++ docker.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+
+# 필수 패키지 설치
+sudo apt install -y apt-transport-https ca-certificates curl software-properties-common
+
+# Docker GPG 키 추가
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
+
+# Docker 저장소 추가
+sudo add-apt-repository -y "deb [arch=amd64] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable"
+
+# NVIDIA Docker GPG 키 추가
+distribution=$(. /etc/os-release;echo $ID$VERSION_ID) \
+   && curl -s -L https://nvidia.github.io/nvidia-docker/gpgkey | sudo apt-key add - \
+   && curl -s -L https://nvidia.github.io/nvidia-docker/$distribution/nvidia-docker.list | sudo tee /etc/apt/sources.list.d/nvidia-docker.list
+
+# 패키지 목록 업데이트
+sudo apt update
+
+# NVIDIA Docker 및 도커 설치
+sudo apt install -y docker-ce docker-ce-cli containerd.io docker-compose-plugin nvidia-docker2
+
+# Docker 데몬 시작
+sudo systemctl enable docker --now
+
+# Docker 권한 부여
+sudo usermod -aG docker user 
+newgrp docker
+
+
